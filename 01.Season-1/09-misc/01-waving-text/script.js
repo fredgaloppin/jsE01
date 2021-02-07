99844b5876604d1021b3ac4eb07ca8c3eee94dfe
root <fredgaloppin@gmail.com>
2021-02-07 08:40:40 +0100

    9.1

--- 01.Season-1/09-misc/01-waving-text/script.js
+++ 01.Season-1/09-misc/01-waving-text/script.js
@@ -2,7 +2,38 @@
 
 
 (() => {
-
-    // your code here
+    let txt = document.querySelector("#target").textContent.split('');
+    target.innerHTML = "";
+   
+    for (let j = 0; j < txt.length; j++) {
+        if (j==0 || j%8 ==0) {
+            let element = txt[j];
+            let result = element.fontsize(7);
+            document.getElementById("target").innerHTML += result;
+        }
+        if (j==1 || j%8 ==1 || j%8 ==7) {
+            let element = txt[j];
+            let result = element.fontsize(6);
+            document.getElementById("target").innerHTML += result;
+        }
+        if (j==2 || j%8 ==2 || j%8 ==6) {
+            let element = txt[j];
+            let result = element.fontsize(5);
+            document.getElementById("target").innerHTML += result; 
+        } 
+        if (j==3 || j%8 ==3 || j%8 ==5) {
+            let element = txt[j];
+            let result = element.fontsize(4);
+            document.getElementById("target").innerHTML += result; 
+            
+        }              
+        if (j==4 || j%8 ==4) {
+            let element = txt[j];
+            let result = element.fontsize(2);
+            document.getElementById("target").innerHTML += result; 
+        }
+    }
 
 })();
+
+        
